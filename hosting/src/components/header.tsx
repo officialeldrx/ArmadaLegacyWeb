bcb6215f6945c75d8ad6e749fb1db65ef44f1a1f
Forrest Weigel <forrest.weigel@gmail.com>
2024-09-15 10:41:54 -0500

    NextJS and ShadCN init

--- hosting/src/components/header.tsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@mui/material";
-
-export default function Header(props: any){
-    return(
-        <div style={{marginTop: '32px', borderTop: '2px solid #24408F', borderBottom: '2px solid #24408F', padding: '1px 0px'}}>
-            <Typography fontFamily={'Trajan Pro'} fontSize={24} textAlign={"center"} style={{color: '#24408F', textTransform: 'uppercase', borderTop: '1px solid #24408F', borderBottom: '1px solid #24408F', paddingTop: '4px'}}>{props.text}</Typography>
-        </div>
-    )
-}
